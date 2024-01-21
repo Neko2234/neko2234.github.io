1ba730323b3e39bba078b386887e152f65648da8
MizutaniKei <99799346+Neko2234@users.noreply.github.com>
2024-01-21 10:04:34 +0900

    first commit

---
+++ test-site/scripts/main.js
@@ -0,0 +1,33 @@
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/2539.png") {
+    myImage.setAttribute("src", "images/amcr.png");
+  } else {
+    myImage.setAttribute("src", "images/2539.png");
+  }
+};
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+function setUserName() {
+  const myName = prompt("あなたの名前を入力してください。");
+  if (myName === null || myName === "") {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `ニゴミクはかわいいよ、${myName} さん、ニゴミクはかわいいよ。`;
+  }
+}
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `ニゴミクはかわいいよ、${storedName}`;
+}
+
+myButton.onclick = () => {
+  setUserName();
+};
